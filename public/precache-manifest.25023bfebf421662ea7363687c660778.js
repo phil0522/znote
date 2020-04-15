c0bc55011770982aac589c21f538754d5dd175dc
phil <phil@example.com>
2020-04-14 17:22:13 -0700

    embed a markdown server

---
+++ public/precache-manifest.25023bfebf421662ea7363687c660778.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ddab3a574d4d5bf6cd14d437426d9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ae391298afa19434d0",
+    "url": "/static/css/2.aa61572a.chunk.css"
+  },
+  {
+    "revision": "7107931b8a842d567bfb",
+    "url": "/static/css/main.7618bff8.chunk.css"
+  },
+  {
+    "revision": "a2ae391298afa19434d0",
+    "url": "/static/js/2.3fe00caf.chunk.js"
+  },
+  {
+    "revision": "487de3e9fbbefa582c7130550f81802f",
+    "url": "/static/js/2.3fe00caf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7107931b8a842d567bfb",
+    "url": "/static/js/main.10cc23a4.chunk.js"
+  },
+  {
+    "revision": "cb1654755c399d554bf6",
+    "url": "/static/js/runtime-main.b92e78a1.js"
+  }
+]);
