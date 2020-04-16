7ce2b0353ded20e63b98679d176eb399b86bd81d
phil <phil@example.com>
2020-04-16 09:26:21 -0700

    auto push

--- pkg/subcommands/commit.go
+++ pkg/subcommands/commit.go
@@ -2,6 +2,7 @@ package subcommands
 
 import (
 	"fmt"
+	"math/rand"
 	"os/exec"
 
 	"github.com/phil0522/znote/pkg/notesmarket"
@@ -15,5 +16,20 @@ func maybeSubmit() {
 
 	if err := cmd.Run(); err != nil {
 		logrus.WithError(err).Warn("Failed to commit")
+	}
+	maybePush()
+}
+
+func maybePush() {
+	r := rand.Intn(100)
+	if r != 75 {
+		return
+	}
+	cmdStr := fmt.Sprintf("cd %s && git push", notesmarket.RootDir)
+
+	cmd := exec.Command("/bin/zsh", "-c", cmdStr)
+
+	if err := cmd.Run(); err != nil {
+		logrus.WithError(err).Warn("Failed to push")
 	}
 }
